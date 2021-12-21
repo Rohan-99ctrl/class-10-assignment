3f192e370986ce80187887f5b3d71dfefc946940
Rohan-99ctrl <rohonmostofa9900@gmail.com>
2021-12-21 21:23:09 +0600

    app.js file upload

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 let emailValid = 'rohanmostofa11@gmail.com';
 
 // Regular Expression pattern one
-let pattern = /^[a-z0-9._-]*@[a-z0-9]*\.[a-z]{2,10}$/;
+let pattern = /^[a-z0-9._-]{1,}@[a-z0-9]{1,}\.[a-z]{2,10}$/;
 
 console.log(pattern.test(emailValid));
 
